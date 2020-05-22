84b518aa533d2f6369087d1badc5be130ee78282
sea5727 <sea5727@naver.com>
2020-05-23 00:48:01 +0900

    commit

---
+++ flutter_fetch_data_from_the_internet/android/app/src/main/kotlin/com/example/flutter_fetch_data_from_the_internet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fetch_data_from_the_internet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_work_with_web_sockets/android/app/src/main/kotlin/com/example/flutter_work_with_web_sockets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_work_with_web_sockets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
