f38faa62883473d8660177219d00576250c2d8cd
sea5727 <sea5727@naver.com>
2020-05-22 22:10:07 +0900

    first-commit

---
+++ flutter_navigator_to_reload_when_popping/android/app/src/main/kotlin/com/example/flutter_navigator_to_reload_when_popping/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_navigator_to_reload_when_popping
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_navigator_to_reload_when_popping/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.green,
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            onPressed: () => Navigator.pop(context),
+            child: new Text("back"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new FirstPageState();
+}
+
+class FirstPageState extends State<FirstPage> {
+
+  Color color = Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: color,
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+              child: new Text("next"),
+              onPressed: () {
+                Navigator
+                    .push(
+                  context,
+                  new MaterialPageRoute(builder: (context) => new SecondPage()),
+                )
+                    .then((value) {
+                  setState(() {
+                    color = color == Colors.white ? Colors.grey : Colors.white;
+                  });
+                });
+              }),
+        ],
+      ),
+    );
+  }
+}
+
+void main() => runApp(
+      new MaterialApp(
+        builder: (context, child) => new SafeArea(child: child),
+        home: new FirstPage(),
+      ),
+    );
